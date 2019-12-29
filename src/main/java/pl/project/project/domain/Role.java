59d4e03da13c2d746a6a1565e1a39cb86d24e31c
Mateusz Gliwka <gliwson@gmail.com>
2019-12-29 19:07:59 +0100

    added entity

--- src/main/java/pl/project/project/domain/Role.java
+++ src/main/java/pl/project/project/domain/Role.java
@@ -1,4 +1,7 @@
 package pl.project.project.domain;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Role {
 }
--- src/main/java/pl/project/project/domain/User.java
+++ src/main/java/pl/project/project/domain/User.java
@@ -1,4 +1,32 @@
 package pl.project.project.domain;
 
+
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import java.util.Collection;
+
+@Entity
+@Data
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String login;
+    @Column(nullable = false)
+    private String password;
+    @Email
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String nick;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Collection<Role> role;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Collection<Project> projects;
 }
