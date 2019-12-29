49dc7a14687526ee444e52187e7b6694b0032225
Mateusz Gliwka <gliwson@gmail.com>
2019-12-29 23:02:11 +0100

    added entity

--- src/main/java/pl/project/project/domain/Project.java
+++ src/main/java/pl/project/project/domain/Project.java
@@ -1,7 +1,19 @@
 package pl.project.project.domain;
 
-import javax.persistence.Entity;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Collection;
 
 @Entity
+@Data
 public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToMany(cascade = CascadeType.ALL, mappedBy = "project")
+    private Collection<User> user;
+
 }
--- src/main/java/pl/project/project/domain/User.java
+++ src/main/java/pl/project/project/domain/User.java
@@ -5,6 +5,7 @@ import lombok.Data;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
 import java.util.Collection;
 
 @Entity
@@ -14,19 +15,29 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
     @Column(nullable = false, unique = true)
     private String login;
+
+    @NotNull
     @Column(nullable = false)
     private String password;
+
     @Email
+    @NotNull
     @Column(nullable = false, unique = true)
     private String email;
+
+    @NotNull
     @Column(nullable = false)
     private String nick;
 
-    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE,
+            CascadeType.DETACH, CascadeType.REFRESH})
     private Collection<Role> role;
 
-    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    private Collection<Project> projects;
+    @ManyToMany(cascade = CascadeType.ALL)
+    private Collection<Project> project;
+
 }
