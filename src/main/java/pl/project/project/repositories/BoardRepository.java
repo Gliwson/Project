9cb3a483110669fc40f20446c9cbbbad3b19a1c6
Mateusz Gliwka <gliwson@gmail.com>
2020-01-07 18:15:00 +0100

    changed application.properties and renamed BoardRepository.java

--- src/main/java/pl/project/project/repositories/BoardRepository.java
+++ src/main/java/pl/project/project/repositories/BoardRepository.java
@@ -2,7 +2,8 @@ package pl.project.project.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import pl.project.project.domain.Board;
 
 @Repository
-public interface Board extends JpaRepository<Board, Long> {
+public interface BoardRepository extends JpaRepository<Board, Long> {
 }
