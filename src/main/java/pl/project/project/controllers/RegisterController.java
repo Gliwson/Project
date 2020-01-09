b60a0000d2a74fa9b28994942d15a3529a0d3674
Mateusz Gliwka <gliwson@gmail.com>
2020-01-09 12:49:55 +0100

    added boostrap

--- src/main/java/pl/project/project/controllers/RegisterController.java
+++ src/main/java/pl/project/project/controllers/RegisterController.java
@@ -1,7 +1,9 @@
 package pl.project.project.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +20,12 @@ public class RegisterController {
 
     public RegisterController(UserService userService) {
         this.userService = userService;
+    }
+
+    @GetMapping("/register")
+    public String showRegisterForm(Model model) {
+        model.addAttribute("user", new UserDTO());
+        return "register";
     }
 
     @PostMapping("/register")
--- src/main/java/pl/project/project/controllers/dtos/UserDTO.java
+++ src/main/java/pl/project/project/controllers/dtos/UserDTO.java
@@ -2,13 +2,20 @@ package pl.project.project.controllers.dtos;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 @Data
 public class UserDTO {
 
+    @NotBlank
     private String login;
 
+    @NotBlank
     private String password;
 
+    @Email
+    @NotBlank
     private String email;
 
 }
